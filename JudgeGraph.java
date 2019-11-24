a1ad021a34da7c37cf4b91f1e02403f8be23a969
tawnyandrews <47125312+tawnyandrews@users.noreply.github.com>
2019-11-23 21:08:58 -0800

    Update JudgeGraph.java

--- JudgeGraph.java
+++ JudgeGraph.java
@@ -1,1 +1,26 @@
 import java.util.*;
+
+public class JudgeGraph{
+  
+    private int verticeCount; 
+    private int edgeCount; 
+
+    public ArrayList<Integer>[] adjacents; 
+
+    //constructer for initializations
+    public JudgeGraph(int verticeCount){
+        this.verticeCount = verticeCount;
+        this.edgeCount = edgeCount;
+        adjacents = new ArrayList [verticeCount];
+        
+        for(int i = 0; i < verticeCount; i++){
+            adjacents[i] = new ArrayList<Integer>();
+        }
+    }
+
+    public void addEdge(int a, int b){ //adds in edges
+        adjacents[a].add(b);
+        edgeCount++; 
+    }
+
+}
